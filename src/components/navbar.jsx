b02391dae3c09ef6c8284726078f4dd993ea617f
Josh <jmart790@fiu.edu>
2019-09-08 18:59:30 -0400

    added class names on elements for styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import '../styles/navbar.css'
 
 const Navbar = props => {
+  const { count, text } = props.countText
   return (
-    <nav>
-      <h1>To-Do</h1>
-      <strong>{props.countText}</strong>
+    <nav className='nav-container'>
+      <span id='nav-title'>To-Do</span>
+      <div id='items-counter'>
+        <div className='nav-counter-container'>
+          <strong>{count}</strong>
+        </div>
+        <div className='nav-text-container'>
+          <strong>{text}</strong>
+        </div>
+      </div>
     </nav>
   )
 }
